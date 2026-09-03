4b8e68297638f0d69afee4ab30f0d0c406cbc465
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-03 10:59:54 -0300

    M11.1 expose interface boundary package

---
+++ src/interface/__init__.py
@@ -0,0 +1,5 @@
+"""M11 interface boundary package."""
+
+from .boundary import InterfaceBoundary, InterfaceChannel, InterfaceRequest, InterfaceResponse
+
+__all__ = ["InterfaceBoundary", "InterfaceChannel", "InterfaceRequest", "InterfaceResponse"]
