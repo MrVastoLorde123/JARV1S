a5e11a690f3c12aff7a7960451493b93ff8cea37
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-03 11:08:40 -0300

    Export M11.2 session runtime

--- src/interface/__init__.py
+++ src/interface/__init__.py
@@ -1,5 +1,16 @@
 """M11 interface boundary package."""
 
 from .boundary import InterfaceBoundary, InterfaceChannel, InterfaceRequest, InterfaceResponse
+from .session import ConversationSession, ConversationTurn, SessionConflictError, SessionRuntime, SessionStore
 
-__all__ = ["InterfaceBoundary", "InterfaceChannel", "InterfaceRequest", "InterfaceResponse"]
+__all__ = [
+    "ConversationSession",
+    "ConversationTurn",
+    "InterfaceBoundary",
+    "InterfaceChannel",
+    "InterfaceRequest",
+    "InterfaceResponse",
+    "SessionConflictError",
+    "SessionRuntime",
+    "SessionStore",
+]
