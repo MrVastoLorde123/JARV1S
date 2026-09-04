7091647a7d810a5fe56ca644680cc161c5c0c44f
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-03 22:32:33 -0300

    Add M15.5 proactive scheduling boundary

---
+++ src/proactive_scheduling.py
@@ -0,0 +1,5 @@
+"""M15.5 proactive scheduling boundary.
+
+Scheduling determines when an initiative proposal may be surfaced or revisited.
+It does not authorize, confirm, or execute the proposed action.
+"""
