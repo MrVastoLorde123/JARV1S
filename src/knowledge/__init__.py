dff0a2d120c0cdc1c82ac447f167fbf0f21ae01d
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-03 21:11:25 -0300

    Export M13.2 identity resolution contracts

--- src/knowledge/__init__.py
+++ src/knowledge/__init__.py
@@ -9,13 +9,31 @@ from .entities import (
     MAX_ENTITY_ID_LENGTH,
     MAX_ENTITY_NAME_LENGTH,
 )
+from .identity import (
+    EntityIdentityResolver,
+    IdentityResolution,
+    IdentityResolutionError,
+    IdentityResolutionStatus,
+    MAX_CANDIDATES,
+    MAX_REASON_LENGTH,
+    MAX_REFERENCE_LENGTH,
+    normalize_identity_reference,
+)
 
 __all__ = [
     "Entity",
     "EntityType",
     "EntityValidationError",
     "MAX_ENTITY_EVIDENCE_REFS",
-    "MAX_ENTITY_ID_LENGTH",
     "MAX_ENTITY_METADATA_ITEMS",
+    "MAX_ENTITY_ID_LENGTH",
     "MAX_ENTITY_NAME_LENGTH",
+    "EntityIdentityResolver",
+    "IdentityResolution",
+    "IdentityResolutionError",
+    "IdentityResolutionStatus",
+    "MAX_CANDIDATES",
+    "MAX_REASON_LENGTH",
+    "MAX_REFERENCE_LENGTH",
+    "normalize_identity_reference",
 ]
