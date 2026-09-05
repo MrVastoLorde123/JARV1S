0212afdf839994a29c3b2e8ae5631b855634d72c
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-05 09:52:58 -0300

    M22.1: expose plugin ecosystem contracts

---
+++ src/plugins/__init__.py
@@ -0,0 +1,5 @@
+"""Bounded JARVIS plugin/capability ecosystem contracts."""
+
+from .ecosystem import CapabilityDescriptor, CapabilityRegistry, PluginRegistryError
+
+__all__ = ["CapabilityDescriptor", "CapabilityRegistry", "PluginRegistryError"]
