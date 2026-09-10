74566ac12acfc86580ba7f1411c643cd58bc2ad5
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-10 02:37:06 -0300

    M27.30: load SELF runtime surface styling

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -16,6 +16,7 @@ import './interface-refinement-m27.27.css';
 import './mission-flow-m27.28.css';
 import './live-missions-footer-m27.30.css';
 import './runtime-state-expression-m27.30.css';
+import './self-runtime-m27.30.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><App /></React.StrictMode>,
