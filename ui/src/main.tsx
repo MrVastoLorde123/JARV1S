e25257e0a6164c7db3dba34b300b673fde2f06d6
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 08:55:44 -0300

    M28.6: integrate shared work scene

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppWorld from './JarvisWorldPrototype';
 import JarvisWorldGuide from './JarvisWorldGuide';
+import JarvisWorldWorkScene from './JarvisWorldWorkScene';
 import './jarvis-world-prototype.css';
 import './jarvis-world-prototype-refinement.css';
 import './jarvis-world-guide.css';
 import './jarvis-world-follow.css';
+import './jarvis-world-work-scene.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppWorld />
+    <JarvisWorldWorkScene />
     <JarvisWorldGuide />
   </React.StrictMode>,
 );
