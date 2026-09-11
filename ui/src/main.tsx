21214b34fe1839aa9ce9f6ba14b30dc9dd755d20
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-10 21:22:04 -0300

    M28: load adaptive JARVIS OS refinement

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppOS from './JarvisOSPrototype';
 import './jarvis-os-prototype.css';
+import './jarvis-os-prototype-refinement.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppOS /></React.StrictMode>,
