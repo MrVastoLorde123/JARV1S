6dcf19018568b1f102864b50797e539fe7513862
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 09:21:24 -0300

    M28.7: integrate JARVIS return briefing

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,6 +4,7 @@ import AppWorld from './JarvisWorldPrototype';
 import JarvisWorldWorkScene from './JarvisWorldWorkScene';
 import JarvisLandscape from './JarvisLandscape';
 import JarvisWorldGuide from './JarvisWorldGuide';
+import JarvisReturnBriefing from './JarvisReturnBriefing';
 import './jarvis-world-prototype.css';
 import './jarvis-world-prototype-refinement.css';
 import './jarvis-world-follow.css';
@@ -11,6 +12,7 @@ import './jarvis-world-work-scene.css';
 import './jarvis-landscape.css';
 import './jarvis-world-breathing.css';
 import './jarvis-world-guide.css';
+import './jarvis-return-briefing.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -18,5 +20,6 @@ createRoot(document.getElementById('root')!).render(
     <JarvisLandscape />
     <JarvisWorldWorkScene />
     <JarvisWorldGuide />
+    <JarvisReturnBriefing />
   </React.StrictMode>,
 );
