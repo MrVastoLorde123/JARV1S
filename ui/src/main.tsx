e0b11cff2e06bfd16b00281857aad333ffaf4785
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 09:43:29 -0300

    M28.8: mount shared JARVIS process world

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import AppWorld from './JarvisWorldPrototype';
 import JarvisWorldWorkScene from './JarvisWorldWorkScene';
 import JarvisLandscape from './JarvisLandscape';
+import JarvisWorldProcess from './JarvisWorldProcess';
 import JarvisWorldGuide from './JarvisWorldGuide';
 import JarvisReturnBriefing from './JarvisReturnBriefing';
 import './jarvis-world-prototype.css';
@@ -11,6 +12,7 @@ import './jarvis-world-follow.css';
 import './jarvis-world-work-scene.css';
 import './jarvis-landscape.css';
 import './jarvis-world-breathing.css';
+import './jarvis-world-process.css';
 import './jarvis-world-guide.css';
 import './jarvis-return-briefing.css';
 
@@ -18,6 +20,7 @@ createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppWorld />
     <JarvisLandscape />
+    <JarvisWorldProcess />
     <JarvisWorldWorkScene />
     <JarvisWorldGuide />
     <JarvisReturnBriefing />
