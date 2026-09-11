44d14ba846fc4576a112dae8751b5180d5e39cc0
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-10 21:22:26 -0300

    M28.1: wire state-driven JARVIS OS behavior

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import AppOS from './JarvisOSPrototype';
 import './jarvis-os-prototype.css';
 import './jarvis-os-prototype-refinement.css';
+import './jarvis-os-state-behavior.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppOS /></React.StrictMode>,
