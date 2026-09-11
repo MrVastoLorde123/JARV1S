ecc62b596489012c30604d13a26be57a9a0bcd1f
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 05:53:32 -0300

    M28.3: wire living world refinement layer

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppWorld from './JarvisWorldPrototype';
+import './jarvis-world-prototype-refinement.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode><AppWorld /></React.StrictMode>,
