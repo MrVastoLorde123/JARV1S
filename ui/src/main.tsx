d934ac1a5cb2cfa88658bb09d9631f0ab7b214fe
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 06:05:36 -0300

    M28.4: mount ambient JARVIS guidance

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppWorld from './JarvisWorldPrototype';
+import JarvisWorldGuide from './JarvisWorldGuide';
+import './jarvis-world-prototype.css';
 import './jarvis-world-prototype-refinement.css';
+import './jarvis-world-guide.css';
 
 createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><AppWorld /></React.StrictMode>,
+  <React.StrictMode>
+    <AppWorld />
+    <JarvisWorldGuide />
+  </React.StrictMode>,
 );
