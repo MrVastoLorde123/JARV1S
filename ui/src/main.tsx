519e58aefa99387bd768f169a739ef402b9a9644
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-11 09:20:02 -0300

    M28.7: integrate landscape and persistent JARVIS presence

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import AppWorld from './JarvisWorldPrototype';
-import JarvisWorldGuide from './JarvisWorldGuide';
 import JarvisWorldWorkScene from './JarvisWorldWorkScene';
+import JarvisLandscape from './JarvisLandscape';
+import JarvisWorldGuide from './JarvisWorldGuide';
 import './jarvis-world-prototype.css';
 import './jarvis-world-prototype-refinement.css';
-import './jarvis-world-guide.css';
 import './jarvis-world-follow.css';
 import './jarvis-world-work-scene.css';
+import './jarvis-landscape.css';
+import './jarvis-world-guide.css';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppWorld />
+    <JarvisLandscape />
     <JarvisWorldWorkScene />
     <JarvisWorldGuide />
   </React.StrictMode>,
