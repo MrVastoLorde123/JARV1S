c05d3fd38daa83e973455c6bde31b0bd59145877
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-12 22:32:28 -0300

    M67: export durable reasoning run loop

--- src/runtime/__init__.py
+++ src/runtime/__init__.py
@@ -1,97 +1,19 @@
 """JARVIS V1 application-layer runtime components."""
 
 from .autonomous_ai_reasoning_provider import AutonomousAIReasoningProvider
-from .autonomous_job import (
-    AutonomousJob,
-    AutonomousJobEvent,
-    AutonomousJobEventKind,
-    AutonomousJobStatus,
-    AutonomousJobStep,
-    AutonomousJobValidationError,
-)
-from .autonomous_job_driver import (
-    AutonomousCycleDisposition,
-    AutonomousCycleResult,
-    AutonomousJobDriver,
-    AutonomousJobDriverValidationError,
-    AutonomousJobWorker,
-)
-from .autonomous_job_execution_pulse import (
-    AutonomousJobExecutionPulse,
-    AutonomousJobExecutionPulseResult,
-)
-from .autonomous_job_persistence import (
-    AutonomousJobPersistenceReceipt,
-    AutonomousJobPersistenceService,
-    AutonomousJobPersistenceValidationError,
-    AutonomousJobStore,
-)
-from .autonomous_job_resume import (
-    AutonomousJobResumeBoundary,
-    AutonomousJobResumeKind,
-    AutonomousJobResumeRequest,
-    AutonomousJobResumeResult,
-)
-from .autonomous_reasoning_action import (
-    AutonomousReasoningAction,
-    AutonomousReasoningActionValidationError,
-    AutonomousReasoningDisposition,
-)
-from .autonomous_reasoning_feedback_pulse import (
-    AutonomousReasoningFeedbackPulse,
-    AutonomousReasoningFeedbackPulseResult,
-)
-from .autonomous_reasoning_tool_gate import (
-    AutonomousReasoningToolGate,
-    AutonomousReasoningToolGateResult,
-)
-from .autonomous_reasoning_tool_feedback_cycle import (
-    AutonomousReasoningToolFeedbackCycle,
-    AutonomousReasoningToolFeedbackCycleCoordinator,
-)
-from .autonomous_resume_reasoning_handoff import (
-    AutonomousResumeReasoningHandoff,
-    AutonomousResumeReasoningHandoffResult,
-)
-from .autonomous_tool_result_feedback import (
-    AutonomousToolResultFeedback,
-    AutonomousToolResultFeedbackAdapter,
-)
+from .autonomous_job import AutonomousJob, AutonomousJobEvent, AutonomousJobEventKind, AutonomousJobStatus, AutonomousJobStep, AutonomousJobValidationError
+from .autonomous_job_driver import AutonomousCycleDisposition, AutonomousCycleResult, AutonomousJobDriver, AutonomousJobDriverValidationError, AutonomousJobWorker
+from .autonomous_job_execution_pulse import AutonomousJobExecutionPulse, AutonomousJobExecutionPulseResult
+from .autonomous_job_persistence import AutonomousJobPersistenceReceipt, AutonomousJobPersistenceService, AutonomousJobPersistenceValidationError, AutonomousJobStore
+from .autonomous_job_resume import AutonomousJobResumeBoundary, AutonomousJobResumeKind, AutonomousJobResumeRequest, AutonomousJobResumeResult
+from .autonomous_reasoning_action import AutonomousReasoningAction, AutonomousReasoningActionValidationError, AutonomousReasoningDisposition
+from .autonomous_reasoning_feedback_pulse import AutonomousReasoningFeedbackPulse, AutonomousReasoningFeedbackPulseResult
+from .autonomous_reasoning_tool_gate import AutonomousReasoningToolGate, AutonomousReasoningToolGateResult
+from .autonomous_reasoning_tool_feedback_cycle import AutonomousReasoningToolFeedbackCycle, AutonomousReasoningToolFeedbackCycleCoordinator
+from .autonomous_resume_reasoning_handoff import AutonomousResumeReasoningHandoff, AutonomousResumeReasoningHandoffResult
+from .autonomous_reasoning_run_loop import AutonomousReasoningRunLoop, AutonomousReasoningRunResult
+from .autonomous_tool_result_feedback import AutonomousToolResultFeedback, AutonomousToolResultFeedbackAdapter
 
 __all__ = [
-    "AutonomousAIReasoningProvider",
-    "AutonomousCycleDisposition",
-    "AutonomousCycleResult",
-    "AutonomousJob",
-    "AutonomousJobDriver",
-    "AutonomousJobDriverValidationError",
-    "AutonomousJobEvent",
-    "AutonomousJobEventKind",
-    "AutonomousJobExecutionPulse",
-    "AutonomousJobExecutionPulseResult",
-    "AutonomousJobPersistenceReceipt",
-    "AutonomousJobPersistenceService",
-    "AutonomousJobPersistenceValidationError",
-    "AutonomousJobResumeBoundary",
-    "AutonomousJobResumeKind",
-    "AutonomousJobResumeRequest",
-    "AutonomousJobResumeResult",
-    "AutonomousJobStatus",
-    "AutonomousJobStep",
-    "AutonomousJobStore",
-    "AutonomousJobValidationError",
-    "AutonomousJobWorker",
-    "AutonomousReasoningAction",
-    "AutonomousReasoningActionValidationError",
-    "AutonomousReasoningDisposition",
-    "AutonomousReasoningFeedbackPulse",
-    "AutonomousReasoningFeedbackPulseResult",
-    "AutonomousReasoningToolGate",
-    "AutonomousReasoningToolGateResult",
-    "AutonomousReasoningToolFeedbackCycle",
-    "AutonomousReasoningToolFeedbackCycleCoordinator",
-    "AutonomousResumeReasoningHandoff",
-    "AutonomousResumeReasoningHandoffResult",
-    "AutonomousToolResultFeedback",
-    "AutonomousToolResultFeedbackAdapter",
+    "AutonomousAIReasoningProvider", "AutonomousCycleDisposition", "AutonomousCycleResult", "AutonomousJob", "AutonomousJobDriver", "AutonomousJobDriverValidationError", "AutonomousJobEvent", "AutonomousJobEventKind", "AutonomousJobExecutionPulse", "AutonomousJobExecutionPulseResult", "AutonomousJobPersistenceReceipt", "AutonomousJobPersistenceService", "AutonomousJobPersistenceValidationError", "AutonomousJobResumeBoundary", "AutonomousJobResumeKind", "AutonomousJobResumeRequest", "AutonomousJobResumeResult", "AutonomousJobStatus", "AutonomousJobStep", "AutonomousJobStore", "AutonomousJobValidationError", "AutonomousJobWorker", "AutonomousReasoningAction", "AutonomousReasoningActionValidationError", "AutonomousReasoningDisposition", "AutonomousReasoningFeedbackPulse", "AutonomousReasoningFeedbackPulseResult", "AutonomousReasoningToolGate", "AutonomousReasoningToolGateResult", "AutonomousReasoningToolFeedbackCycle", "AutonomousReasoningToolFeedbackCycleCoordinator", "AutonomousResumeReasoningHandoff", "AutonomousResumeReasoningHandoffResult", "AutonomousReasoningRunLoop", "AutonomousReasoningRunResult", "AutonomousToolResultFeedback", "AutonomousToolResultFeedbackAdapter",
 ]
