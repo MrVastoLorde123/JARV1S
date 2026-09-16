53fa22c8137891600fd25f5001ce91cb902d6fcd
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-16 20:10:38 -0300

    M38: export recovery contract without altering existing exports

--- src/agency/__init__.py
+++ src/agency/__init__.py
@@ -1,29 +1,122 @@
 """Agency runtime components for controlled execution, workforce, and world observation."""
 
 from .agent_entity import AgentEntity, AgentLandscape, AgentStatus
-from .agent_runtime import AgentHandoff, AgentRoute, AgentRuntime, EvidenceBundle, EvidenceReference, WorkLineage
-from .driveability import ContinuationCycle, ContinuationDecision, ContinuationStopReason, DriveabilityController, NextStepProposal, Objective, ObjectiveState
-from .execution_runtime import ExecutionAdapter, ExecutionObservation, ExecutionOutcome, ExecutionRuntime, ExecutionStatus
+from .agent_runtime import (
+    AgentHandoff,
+    AgentRoute,
+    AgentRuntime,
+    EvidenceBundle,
+    EvidenceReference,
+    WorkLineage,
+)
+from .driveability import (
+    ContinuationCycle,
+    ContinuationDecision,
+    ContinuationStopReason,
+    DriveabilityController,
+    NextStepProposal,
+    Objective,
+    ObjectiveState,
+)
+from .execution_runtime import (
+    ExecutionAdapter,
+    ExecutionObservation,
+    ExecutionOutcome,
+    ExecutionRuntime,
+    ExecutionStatus,
+)
 from .world_projection import WorldAgentObservation, WorldObservation, WorldObservationProjector
 from .world_runtime import AgentWorldRuntime
 from .work_state import WorkBlocker, WorkRole, WorkStage, WorkState, WorkStatus, infer_work_role
 from .work_planning import PlanStepKind, WorkPlan, WorkPlanStep, build_work_plan, next_ready_steps
-from .task_orchestration import CoordinationAction, CoordinationDecision, OrchestratedStep, OrchestrationStatus, StepExecutionState, TaskOrchestration, begin_task_orchestration, choose_next_coordination, update_orchestration
+from .task_orchestration import (
+    CoordinationAction,
+    CoordinationDecision,
+    OrchestratedStep,
+    OrchestrationStatus,
+    StepExecutionState,
+    TaskOrchestration,
+    begin_task_orchestration,
+    choose_next_coordination,
+    update_orchestration,
+)
 from .work_dispatch import DispatchRequest, DispatchResult, WorkDispatcher
 from .execution_handoff import ExecutionHandoff, create_execution_handoff
 from .execution_bridge import AgencyExecutionBridgeResult, run_execution_handoff
-from .execution_outcome import AgencyExecutionOutcome, AgencyOutcomeStatus, AgencyVerifier, VerificationDecision, VerificationDisposition, VerificationInput, build_verification_input, classify_agency_outcome
+from .execution_outcome import (
+    AgencyExecutionOutcome,
+    AgencyOutcomeStatus,
+    AgencyVerifier,
+    VerificationDecision,
+    VerificationDisposition,
+    VerificationInput,
+    build_verification_input,
+    classify_agency_outcome,
+)
 from .verification_recovery import RecoveryDecision, RecoveryDisposition, derive_recovery_decision
 
 __all__ = [
-    "AgentEntity", "AgentLandscape", "AgentStatus", "AgentHandoff", "AgentRoute", "AgentRuntime", "AgentWorldRuntime",
-    "EvidenceBundle", "EvidenceReference", "WorkLineage", "WorldAgentObservation", "WorldObservation", "WorldObservationProjector",
-    "ContinuationCycle", "ContinuationDecision", "ContinuationStopReason", "DriveabilityController", "NextStepProposal", "Objective", "ObjectiveState",
-    "ExecutionAdapter", "ExecutionObservation", "ExecutionOutcome", "ExecutionRuntime", "ExecutionStatus",
-    "WorkBlocker", "WorkRole", "WorkStage", "WorkState", "WorkStatus", "infer_work_role",
-    "PlanStepKind", "WorkPlan", "WorkPlanStep", "build_work_plan", "next_ready_steps",
-    "CoordinationAction", "CoordinationDecision", "OrchestratedStep", "OrchestrationStatus", "StepExecutionState", "TaskOrchestration", "begin_task_orchestration", "choose_next_coordination", "update_orchestration",
-    "DispatchRequest", "DispatchResult", "WorkDispatcher", "ExecutionHandoff", "create_execution_handoff", "AgencyExecutionBridgeResult", "run_execution_handoff",
-    "AgencyExecutionOutcome", "AgencyOutcomeStatus", "AgencyVerifier", "VerificationDecision", "VerificationDisposition", "VerificationInput", "build_verification_input", "classify_agency_outcome",
-    "RecoveryDecision", "RecoveryDisposition", "derive_recovery_decision",
+    "AgentEntity",
+    "AgentLandscape",
+    "AgentStatus",
+    "AgentHandoff",
+    "AgentRoute",
+    "AgentRuntime",
+    "AgentWorldRuntime",
+    "EvidenceBundle",
+    "EvidenceReference",
+    "WorkLineage",
+    "WorldAgentObservation",
+    "WorldObservation",
+    "WorldObservationProjector",
+    "ContinuationCycle",
+    "ContinuationDecision",
+    "ContinuationStopReason",
+    "DriveabilityController",
+    "NextStepProposal",
+    "Objective",
+    "ObjectiveState",
+    "ExecutionAdapter",
+    "ExecutionObservation",
+    "ExecutionOutcome",
+    "ExecutionRuntime",
+    "ExecutionStatus",
+    "WorkBlocker",
+    "WorkRole",
+    "WorkStage",
+    "WorkState",
+    "WorkStatus",
+    "infer_work_role",
+    "PlanStepKind",
+    "WorkPlan",
+    "WorkPlanStep",
+    "build_work_plan",
+    "next_ready_steps",
+    "CoordinationAction",
+    "CoordinationDecision",
+    "OrchestratedStep",
+    "OrchestrationStatus",
+    "StepExecutionState",
+    "TaskOrchestration",
+    "begin_task_orchestration",
+    "choose_next_coordination",
+    "update_orchestration",
+    "DispatchRequest",
+    "DispatchResult",
+    "WorkDispatcher",
+    "ExecutionHandoff",
+    "create_execution_handoff",
+    "AgencyExecutionBridgeResult",
+    "run_execution_handoff",
+    "AgencyExecutionOutcome",
+    "AgencyOutcomeStatus",
+    "AgencyVerifier",
+    "VerificationDecision",
+    "VerificationDisposition",
+    "VerificationInput",
+    "build_verification_input",
+    "classify_agency_outcome",
+    "RecoveryDecision",
+    "RecoveryDisposition",
+    "derive_recovery_decision",
 ]
