88f0e736192df8fdef0ab4c0b9fbc6f4894af0cc
MrVastoLorde123 <h.jemero0000@gmail.com>
2026-09-17 00:37:14 -0300

    Phase 2: preserve existing agency exports and add learning contracts

--- src/agency/__init__.py
+++ src/agency/__init__.py
@@ -2,33 +2,107 @@
 
 from .agent_entity import AgentEntity, AgentLandscape, AgentStatus
 from .agent_runtime import (
-    AgentHandoff, AgentRoute, AgentRuntime, EvidenceBundle, EvidenceReference, WorkLineage,
+    AgentHandoff,
+    AgentRoute,
+    AgentRuntime,
+    EvidenceBundle,
+    EvidenceReference,
+    WorkLineage,
 )
 from .driveability import (
-    ContinuationCycle, ContinuationDecision, ContinuationStopReason, DriveabilityController,
-    NextStepProposal, Objective, ObjectiveState,
+    ContinuationCycle,
+    ContinuationDecision,
+    ContinuationStopReason,
+    DriveabilityController,
+    NextStepProposal,
+    Objective,
+    ObjectiveState,
 )
-from .execution_runtime import ExecutionAdapter, ExecutionObservation, ExecutionOutcome, ExecutionRuntime, ExecutionStatus
+from .execution_runtime import (
+    ExecutionAdapter,
+    ExecutionObservation,
+    ExecutionOutcome,
+    ExecutionRuntime,
+    ExecutionStatus,
+)
 from .world_projection import WorldAgentObservation, WorldObservation, WorldObservationProjector
 from .world_runtime import AgentWorldRuntime
 from .world_model import WorldModelFact, WorldModelSnapshot, build_world_model_snapshot
-from .world_model_qualification import WorldFactAssessment, WorldFactFreshness, WorldFactQualification, WorldModelQualification, assess_world_model
+from .world_model_qualification import (
+    WorldFactAssessment,
+    WorldFactFreshness,
+    WorldFactQualification,
+    WorldModelQualification,
+    assess_world_model,
+)
 from .current_context import CurrentContext, CurrentContextFact, build_current_context
 from .reasoning import ReasoningHypothesis, ReasoningResult, build_reasoning_result
-from .initiative_candidate import InitiativeCandidate, InitiativeCandidateSet, build_initiative_candidate_set
-from .initiative_evaluation import InitiativeEvaluation, InitiativeEvaluationSet, build_initiative_evaluation_set
-from .information_gain import InformationGainAssessment, InformationGainOpportunity, build_information_gain_assessment
-from .proactive_proposal import ProactiveProposal, ProactiveProposalSet, build_proactive_proposal_set
-from .scheduling_notification import SchedulingNotificationKind, SchedulingNotificationProposal, SchedulingNotificationProposalSet, build_scheduling_notification_proposal_set
-from .confirmation import ConfirmationDisposition, ConfirmationRequest, ConfirmationResult, build_confirmation_result
-from .authorization_decision import AuthorizationDecision, AuthorizationDecisionSet, AuthorizationDisposition, build_authorization_decision_set
-from .authorization_execution_bridge import AuthorizationExecutionBridge, create_authorization_execution_bridge
-from .authorized_execution_admission import AuthorizedExecutionAdmission, create_authorized_execution_admission
-from .authorized_execution_runtime_admission import AuthorizedExecutionRuntimeAdmission, create_authorized_execution_runtime_admission
-from .authorized_execution_outcome import AuthorizedExecutionOutcome, build_authorized_execution_outcome
-from .authorized_execution_verification import AuthorizedExecutionVerification, bind_authorized_execution_verification
-from .authorized_execution_recovery import AuthorizedExecutionRecovery, build_authorized_execution_recovery
-from .authorized_execution_reconciliation import AuthorizedExecutionReconciliation, build_authorized_execution_reconciliation
+from .initiative_candidate import (
+    InitiativeCandidate,
+    InitiativeCandidateSet,
+    build_initiative_candidate_set,
+)
+from .initiative_evaluation import (
+    InitiativeEvaluation,
+    InitiativeEvaluationSet,
+    build_initiative_evaluation_set,
+)
+from .information_gain import (
+    InformationGainAssessment,
+    InformationGainOpportunity,
+    build_information_gain_assessment,
+)
+from .proactive_proposal import (
+    ProactiveProposal,
+    ProactiveProposalSet,
+    build_proactive_proposal_set,
+)
+from .scheduling_notification import (
+    SchedulingNotificationKind,
+    SchedulingNotificationProposal,
+    SchedulingNotificationProposalSet,
+    build_scheduling_notification_proposal_set,
+)
+from .confirmation import (
+    ConfirmationDisposition,
+    ConfirmationRequest,
+    ConfirmationResult,
+    build_confirmation_result,
+)
+from .authorization_decision import (
+    AuthorizationDecision,
+    AuthorizationDecisionSet,
+    AuthorizationDisposition,
+    build_authorization_decision_set,
+)
+from .authorization_execution_bridge import (
+    AuthorizationExecutionBridge,
+    create_authorization_execution_bridge,
+)
+from .authorized_execution_admission import (
+    AuthorizedExecutionAdmission,
+    create_authorized_execution_admission,
+)
+from .authorized_execution_runtime_admission import (
+    AuthorizedExecutionRuntimeAdmission,
+    create_authorized_execution_runtime_admission,
+)
+from .authorized_execution_outcome import (
+    AuthorizedExecutionOutcome,
+    build_authorized_execution_outcome,
+)
+from .authorized_execution_verification import (
+    AuthorizedExecutionVerification,
+    bind_authorized_execution_verification,
+)
+from .authorized_execution_recovery import (
+    AuthorizedExecutionRecovery,
+    build_authorized_execution_recovery,
+)
+from .authorized_execution_reconciliation import (
+    AuthorizedExecutionReconciliation,
+    build_authorized_execution_reconciliation,
+)
 from .experience_feedback import ExperienceFeedback, build_experience_feedback
 from .experience_record import ExperienceRecord, build_experience_record
 from .learning_signal import LearningSignal, build_learning_signal
@@ -39,28 +113,176 @@ from .adaptation_application import AdaptationApplication, LearningProfile, appl
 from .adaptation_outcome import AdaptationOutcome, AdaptationOutcomeDisposition, evaluate_adaptation_outcome
 from .work_state import WorkBlocker, WorkRole, WorkStage, WorkState, WorkStatus, infer_work_role
 from .work_planning import PlanStepKind, WorkPlan, WorkPlanStep, build_work_plan, next_ready_steps
-from .task_orchestration import CoordinationAction, CoordinationDecision, OrchestratedStep, OrchestrationStatus, StepExecutionState, TaskOrchestration, begin_task_orchestration, choose_next_coordination, update_orchestration
+from .task_orchestration import (
+    CoordinationAction,
+    CoordinationDecision,
+    OrchestratedStep,
+    OrchestrationStatus,
+    StepExecutionState,
+    TaskOrchestration,
+    begin_task_orchestration,
+    choose_next_coordination,
+    update_orchestration,
+)
 from .work_dispatch import DispatchRequest, DispatchResult, WorkDispatcher
 from .execution_handoff import ExecutionHandoff, create_execution_handoff
 from .execution_bridge import AgencyExecutionBridgeResult, run_execution_handoff
-from .execution_outcome import AgencyExecutionOutcome, AgencyOutcomeStatus, AgencyVerifier, VerificationDecision, VerificationDisposition, VerificationInput, build_verification_input, classify_agency_outcome
+from .execution_outcome import (
+    AgencyExecutionOutcome,
+    AgencyOutcomeStatus,
+    AgencyVerifier,
+    VerificationDecision,
+    VerificationDisposition,
+    VerificationInput,
+    build_verification_input,
+    classify_agency_outcome,
+)
 from .verification_recovery import RecoveryDecision, RecoveryDisposition, derive_recovery_decision
 from .recovery_state import RecoveryReconciliation, RecoveryStateDisposition, reconcile_recovery
 from .lifecycle_state import AgencyLifecycleState, build_agency_lifecycle_state
 from .lifecycle_integration import AgencyLifecycleIntegration, build_agency_lifecycle_integration
 
 __all__ = [
-    "AgentEntity", "AgentLandscape", "AgentStatus", "AgentHandoff", "AgentRoute", "AgentRuntime", "AgentWorldRuntime", "EvidenceBundle", "EvidenceReference", "WorkLineage",
-    "WorldAgentObservation", "WorldObservation", "WorldObservationProjector", "WorldModelFact", "WorldModelSnapshot", "build_world_model_snapshot",
-    "WorldFactAssessment", "WorldFactFreshness", "WorldFactQualification", "WorldModelQualification", "assess_world_model", "CurrentContext", "CurrentContextFact", "build_current_context",
-    "ReasoningHypothesis", "ReasoningResult", "build_reasoning_result", "InitiativeCandidate", "InitiativeCandidateSet", "build_initiative_candidate_set", "InitiativeEvaluation", "InitiativeEvaluationSet", "build_initiative_evaluation_set",
-    "InformationGainAssessment", "InformationGainOpportunity", "build_information_gain_assessment", "ProactiveProposal", "ProactiveProposalSet", "build_proactive_proposal_set", "SchedulingNotificationKind", "SchedulingNotificationProposal", "SchedulingNotificationProposalSet", "build_scheduling_notification_proposal_set",
-    "ConfirmationDisposition", "ConfirmationRequest", "ConfirmationResult", "build_confirmation_result", "AuthorizationDecision", "AuthorizationDecisionSet", "AuthorizationDisposition", "build_authorization_decision_set", "AuthorizationExecutionBridge", "create_authorization_execution_bridge",
-    "AuthorizedExecutionAdmission", "create_authorized_execution_admission", "AuthorizedExecutionRuntimeAdmission", "create_authorized_execution_runtime_admission", "AuthorizedExecutionOutcome", "build_authorized_execution_outcome", "AuthorizedExecutionVerification", "bind_authorized_execution_verification",
-    "AuthorizedExecutionRecovery", "build_authorized_execution_recovery", "AuthorizedExecutionReconciliation", "build_authorized_execution_reconciliation", "ExperienceFeedback", "build_experience_feedback", "ExperienceRecord", "build_experience_record",
-    "LearningSignal", "build_learning_signal", "LearningEvaluation", "LearningEvaluationDisposition", "evaluate_learning_signal", "AdaptationProposal", "propose_adaptation", "AdaptationValidation", "AdaptationValidationDisposition", "validate_adaptation",
-    "AdaptationApplication", "LearningProfile", "apply_adaptation", "AdaptationOutcome", "AdaptationOutcomeDisposition", "evaluate_adaptation_outcome", "ContinuationCycle", "ContinuationDecision", "ContinuationStopReason", "DriveabilityController", "NextStepProposal", "Objective", "ObjectiveState",
-    "ExecutionAdapter", "ExecutionObservation", "ExecutionOutcome", "ExecutionRuntime", "ExecutionStatus", "WorkBlocker", "WorkRole", "WorkStage", "WorkState", "WorkStatus", "infer_work_role", "PlanStepKind", "WorkPlan", "WorkPlanStep", "build_work_plan", "next_ready_steps",
-    "CoordinationAction", "CoordinationDecision", "OrchestratedStep", "OrchestrationStatus", "StepExecutionState", "TaskOrchestration", "begin_task_orchestration", "choose_next_coordination", "update_orchestration", "DispatchRequest", "DispatchResult", "WorkDispatcher", "ExecutionHandoff", "create_execution_handoff", "AgencyExecutionBridgeResult", "run_execution_handoff",
-    "AgencyExecutionOutcome", "AgencyOutcomeStatus", "AgencyVerifier", "VerificationDecision", "VerificationDisposition", "VerificationInput", "build_verification_input", "classify_agency_outcome", "RecoveryDecision", "RecoveryDisposition", "derive_recovery_decision", "RecoveryReconciliation", "RecoveryStateDisposition", "reconcile_recovery", "AgencyLifecycleState", "build_agency_lifecycle_state", "AgencyLifecycleIntegration", "build_agency_lifecycle_integration",
+    "AgentEntity",
+    "AgentLandscape",
+    "AgentStatus",
+    "AgentHandoff",
+    "AgentRoute",
+    "AgentRuntime",
+    "AgentWorldRuntime",
+    "EvidenceBundle",
+    "EvidenceReference",
+    "WorkLineage",
+    "WorldAgentObservation",
+    "WorldObservation",
+    "WorldObservationProjector",
+    "WorldModelFact",
+    "WorldModelSnapshot",
+    "build_world_model_snapshot",
+    "WorldFactAssessment",
+    "WorldFactFreshness",
+    "WorldFactQualification",
+    "WorldModelQualification",
+    "assess_world_model",
+    "CurrentContext",
+    "CurrentContextFact",
+    "build_current_context",
+    "ReasoningHypothesis",
+    "ReasoningResult",
+    "build_reasoning_result",
+    "InitiativeCandidate",
+    "InitiativeCandidateSet",
+    "build_initiative_candidate_set",
+    "InitiativeEvaluation",
+    "InitiativeEvaluationSet",
+    "build_initiative_evaluation_set",
+    "InformationGainAssessment",
+    "InformationGainOpportunity",
+    "build_information_gain_assessment",
+    "ProactiveProposal",
+    "ProactiveProposalSet",
+    "build_proactive_proposal_set",
+    "SchedulingNotificationKind",
+    "SchedulingNotificationProposal",
+    "SchedulingNotificationProposalSet",
+    "build_scheduling_notification_proposal_set",
+    "ConfirmationDisposition",
+    "ConfirmationRequest",
+    "ConfirmationResult",
+    "build_confirmation_result",
+    "AuthorizationDecision",
+    "AuthorizationDecisionSet",
+    "AuthorizationDisposition",
+    "build_authorization_decision_set",
+    "AuthorizationExecutionBridge",
+    "create_authorization_execution_bridge",
+    "AuthorizedExecutionAdmission",
+    "create_authorized_execution_admission",
+    "AuthorizedExecutionRuntimeAdmission",
+    "create_authorized_execution_runtime_admission",
+    "AuthorizedExecutionOutcome",
+    "build_authorized_execution_outcome",
+    "AuthorizedExecutionVerification",
+    "bind_authorized_execution_verification",
+    "AuthorizedExecutionRecovery",
+    "build_authorized_execution_recovery",
+    "AuthorizedExecutionReconciliation",
+    "build_authorized_execution_reconciliation",
+    "ExperienceFeedback",
+    "build_experience_feedback",
+    "ExperienceRecord",
+    "build_experience_record",
+    "LearningSignal",
+    "build_learning_signal",
+    "LearningEvaluation",
+    "LearningEvaluationDisposition",
+    "evaluate_learning_signal",
+    "AdaptationProposal",
+    "propose_adaptation",
+    "AdaptationValidation",
+    "AdaptationValidationDisposition",
+    "validate_adaptation",
+    "AdaptationApplication",
+    "LearningProfile",
+    "apply_adaptation",
+    "AdaptationOutcome",
+    "AdaptationOutcomeDisposition",
+    "evaluate_adaptation_outcome",
+    "ContinuationCycle",
+    "ContinuationDecision",
+    "ContinuationStopReason",
+    "DriveabilityController",
+    "NextStepProposal",
+    "Objective",
+    "ObjectiveState",
+    "ExecutionAdapter",
+    "ExecutionObservation",
+    "ExecutionOutcome",
+    "ExecutionRuntime",
+    "ExecutionStatus",
+    "WorkBlocker",
+    "WorkRole",
+    "WorkStage",
+    "WorkState",
+    "WorkStatus",
+    "infer_work_role",
+    "PlanStepKind",
+    "WorkPlan",
+    "WorkPlanStep",
+    "build_work_plan",
+    "next_ready_steps",
+    "CoordinationAction",
+    "CoordinationDecision",
+    "OrchestratedStep",
+    "OrchestrationStatus",
+    "StepExecutionState",
+    "TaskOrchestration",
+    "begin_task_orchestration",
+    "choose_next_coordination",
+    "update_orchestration",
+    "DispatchRequest",
+    "DispatchResult",
+    "WorkDispatcher",
+    "ExecutionHandoff",
+    "create_execution_handoff",
+    "AgencyExecutionBridgeResult",
+    "run_execution_handoff",
+    "AgencyExecutionOutcome",
+    "AgencyOutcomeStatus",
+    "AgencyVerifier",
+    "VerificationDecision",
+    "VerificationDisposition",
+    "VerificationInput",
+    "build_verification_input",
+    "classify_agency_outcome",
+    "RecoveryDecision",
+    "RecoveryDisposition",
+    "derive_recovery_decision",
+    "RecoveryReconciliation",
+    "RecoveryStateDisposition",
+    "reconcile_recovery",
+    "AgencyLifecycleState",
+    "build_agency_lifecycle_state",
+    "AgencyLifecycleIntegration",
+    "build_agency_lifecycle_integration",
 ]
